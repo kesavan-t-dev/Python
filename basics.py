fa9c14f7ea9a74b9d131190e5145ce638f069bae
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-09 17:40:03 +0530

    add if and else

--- basics.py
+++ basics.py
@@ -118,3 +118,132 @@ print("Data Type of", var_1, "=", type(var_1))
 print("Data Type of", var_2, "=", type(var_2))  
 print("Data Type of", var_3, "=", type(var_3))  
 print("Data Type of", var_4, "=", type(var_4)) 
+
+print()
+
+# python program to show how to declare complex numbers in Python  
+  
+# initializing some variables with complex numbers  
+var_1 = 3 + 5j  # where 2 is the real part and 5 is the imaginary part  
+var_2 = -3.4 + 2.8j  
+var_3 = -5 - 1.9j  
+var_4 = -6 + 0j  
+  
+# printing the types of initialized variables  
+print("Data Type of", var_1, "=", type(var_1))  
+print("Data Type of", var_2, "=", type(var_2))  
+print("Data Type of", var_3, "=", type(var_3))  
+print("Data Type of", var_4, "=", type(var_4))  
+print()
+
+# simple python program to show how to initialize a list  
+  
+# creating a list  
+list_1 = [1, 0.5, 'hello', 1+5j, 1.7e2, -12, 'welcome'] # different types of elements  
+  
+# printing details  
+print("Initialized List:", list_1) # printing elements of the list  
+print("Data Type:", type(list_1)) # printing data type  
+
+print()
+
+# simple python program to show how to initialize a tuple  
+  
+# creating a tuple  
+tuple_1 = (1, 0.5, 'hello', 1+5j, 1.7e2, -12, 'welcome') # different types of elements  
+  
+# printing details  
+print("Initialized tuple:", tuple_1) # printing elements of the tuple  
+print("Data Type:", type(tuple_1)) # printing data type  
+print()
+
+# simple python program to show how to initialize string  
+  
+# initializing variables with some string  
+str_1 = 'TpointTech'  
+str_2 = 'I love learning Python from TpointTech'  
+  
+# printing details  
+print("String 1 :", str_1) # printing string  
+print("Data Type of String 1:", type(str_1)) # printing data type  
+  
+print("String 2 :", str_2) # printing string  
+print("Data Type of String 2:", type(str_2)) # printing data type  
+print()
+
+# simple python program to show how to initialize a range  
+  
+# creating a range  
+r = range(3, 26, 2)  
+  
+# printing the results  
+print("Range:", r) # printing range  
+print("Range as List:", list(r)) # printing range as a list  
+print("Data Type:", type(r)) # printing data type 
+print()
+
+# simple python program to show how to initialize a set  
+  
+# creating sets  
+set_1 = {"mango", "apple", "orange", "banana"} # using {}  
+set_2 = set(["ginger", "lemon", "potato", "tomato"])  
+  
+# printing the results  
+print("Set 1:", set_1) # set 1  
+print("Data type of Set 1:", type(set_1)) # data type of set 1  
+  
+print("Set 2:", set_2) # set 2  
+print("Data type of Set 2:", type(set_2)) # data type of set 2  
+print()
+
+# simple python program to show how to create a frozeset  
+  
+# creating an iterable  
+list_1 = ["berries", "orange", "mango", "apple"]      # a list  
+  
+# using the frozenset() function to create a frozenset object  
+f_1 = frozenset(list_1)  
+  
+# printing results  
+print("Frozenset:", f_1) # frozenset  
+print("Data Type:", type(f_1)) # data type  
+print()
+
+  
+# creating a dictionary  
+person_1 = {  
+    'name' : 'Mark',  
+    'age': '25',  
+    'gender' : 'Male',  
+}  
+  
+# printing results  
+print("Person Details:\n", person_1)  # dictionary  
+print("Data Type:", type(person_1))   # its data type  
+print()
+
+
+# read about 
+# Numbers
+# Type casting
+# string
+# string Methods
+# boolean
+# small check but as per we thought if we use input it has the data type string only it not change if i give number
+#val = input("Enter letter or number")
+#print(type(val))
+#val = input("Enter letter or number")
+#print(type(val))
+# # # # # # # # # ### # # # # # # # # # # # # # # # # # # # ### # # # # # # # # # ## # # # # # # # # ### # # # # # # # # # #
+# now we move to the selection statements 
+# first is if else 
+val = input("Enter a password :")
+if len(val) >= 8:  # checking the length of the password  
+  # outer if block  
+  if any(char.isdigit() for char in val): # checking if the password consists of any numeral value  
+    # inner if block  
+    print("Password is Strong")
+  else:
+    print("password is contain one number")
+else:
+  print("Password is must in above 8 character(s)")
