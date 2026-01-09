c484900f4bfa78ec2fe25451862d6d138b24cfc3
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-09 11:44:25 +0530

    learn about python basics

---
+++ basics.py
@@ -0,0 +1,16 @@
+print("hellow")
+
+name = "my name"
+print(name)
+# it will update the below name initialise
+name = 12
+print(name)
+# it is a dynamic language we change the datatype for their variable names 
+
+#'''This is for indentation for selection statements'''
+
+if 9 > 5:  
+  print("9 is greater than 5")      # proper indentation  
+  print("This is part of the if block")  
+print("This is outside the if block")  
+
