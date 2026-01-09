8a6068259c6a938466046706dcded9527f9e4504
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-09 11:56:28 +0530

    arithmetic operator

--- basics.py
+++ basics.py
@@ -14,3 +14,19 @@ if 9 > 5:
   print("This is part of the if block")  
 print("This is outside the if block")  
 
+# Operators 
+# Arithmetic operator
+# ex
+a = 46    # Initializing the value of a      
+b = 4     # Initializing the value of b      
+    
+print("For a =", a, "and b =", b,"\nCalculate the following:")    
+    
+# printing different results    
+print('1. Addition of two numbers: a + b =', a + b)      
+print('2. Subtraction of two numbers: a - b =', a - b)      
+print('3. Multiplication of two numbers: a * b =', a * b)      
+print('4. Division of two numbers: a / b =', a / b)      
+print('5. Floor division of two numbers: a // b =',a // b)      
+print("""6. Reminder of two numbers: a '%' b =""", a % b)     # '%' for it takes the values for the operator in the single string and triple string so i use the single '  
+print("""7. Exponent of two numbers: a ^/'**' b =""",a ** b)    # '**' it will multiply the two string from before and after 
