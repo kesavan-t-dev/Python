d67310dbdea2407ec69a5c49a5e36681866d03da
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-13 17:32:46 +0530

    add some changes and correct the edge cases

--- tasks/odd_even.py
+++ tasks/odd_even.py
@@ -1,11 +1,11 @@
 #-----ODD & EVEN
 
 #____Method__1
-
-values = int(input("Enter the values :"))
+"""
+values = int(input("Enter the range :"))
 
 def odd_even(n):
-    n = n
+    n = abs(n)
     odd = [] 
     even = [] 
     for i in range(1,n+1):
@@ -19,24 +19,21 @@ def odd_even(n):
 odd,even = odd_even(values)
 print("Odd numbers are :", odd)
 print("Even numbers are :", even)
+"""
 
 #____Method__2
 
-def separate_even_odd(nums):
-    
-    if not nums:
-        return [], []
-    
-    evens, odds = separate_even_odd(nums[1:])
-    
-    if nums[0] % 2 == 0:
-        evens.insert(0, nums[0])
-    else:
-        odds.insert(0, nums[0])
-    
-    return evens, odds
+def separate_even_odd(n):
+    if n < 0:
+        print("give positive range")
+        exit()
 
-numbers = [5, 8, 13, 22, 7]
+    numbers = list(range(1, n + 1))
+    even = [num for num in numbers if num % 2 == 0]
+    odd = [num for num in numbers if num % 2 != 0]
+    return odd, even
+
+numbers = int(input("Enter the range :"))
 even_list, odd_list = separate_even_odd(numbers)
 
 print("Even numbers:", even_list)
