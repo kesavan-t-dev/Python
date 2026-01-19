6350b9b7ab24b8e9b31259385eef10b034c5f83f
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-19 16:11:28 +0530

    added two methods for factorial using while and for loop

--- tasks/factorial.py
+++ tasks/factorial.py
@@ -1,2 +1,31 @@
 ##_____Write a function to print a factorial numbers for a given input range (2 methods )
+#Method_1
+# def factorial(n):
+#     i = n - 1
+#     org = n
+#     while i > 0:
+#         n = n * i
+#         i -=  1
+#     print(f"{n} is a factorial of {org}")
+    
+
+# n = int(input("Enter a number: "))
+# factorial(n)
+
+# #Method_2
+def fact(n):
+    org = n
+    i = n - 1
+    for i in range(n,0,-1):
+        i -= 1
+        if(i == 0):
+            break
+        else:
+            n *= i
+           
+    print(f"{n} is a factorial of {org}")
+    
+
+n = int(input("Enter a number: "))
+fact(n)
 
