f83264820a5b268a6e15fcea52b341f65a41e117
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-19 20:57:03 +0530

    changed logic for palindrome

--- tasks/palindrome.py
+++ tasks/palindrome.py
@@ -15,37 +15,41 @@
 #     print(f"{word} Not Palindrome")
 
 #----Method_2
-# def checks_palind(word):
-#     word = word.lower()  
-#     length = len(word)
-    
-#     for i in range(length // 2):  
-#         if word[i] != word[length - i - 1]:
-#             print(f"{word} is not a palindrome.")
-#             return  
+def checks_palind(word):
+    word = word.lower()  
+    length = len(word) 
+    print("before ",length) 
+    count = 0
+    for i in range(length // 2):  
+        count = i
+        print(count)
+        print("after for ",length//2) 
+        if word[i] != word[length - i - 1]:
+            print(f"{word} is not a palindrome.")
+            return  
    
-#     print(f"{word} is a palindrome.")
+    print(f"{word} is a palindrome.")
 
-
-# word = input("Enter a word: ")
-# checks_palind(word)
-
-#----Method_3
-def check(word):
-    i, j = 0, len(word) - 1  
-    is_palindrome = True  
-    while i < j:
-        if word[i] != word[j]:  
-            is_palindrome = False
-        break
-    i += 1
-    j -= 1
-
-    return is_palindrome
 
 word = input("Enter a word: ")
-a = check(word)
-if(a == True):
-    print(f"{word} is a palindrome.")
-else:
-    print(f"{word} is not a palindrome.")
+checks_palind(word)
+
+# #----Method_3
+# def check(word):
+#     i, j = 0, len(word) - 1  
+#     is_palindrome = True  
+#     while i < j:
+#         if word[i] != word[j]:  
+#             is_palindrome = False
+#         break
+#     i += 1
+#     j -= 1
+
+#     return is_palindrome
+
+# word = input("Enter a word: ")
+# a = check(word)
+# if(a == True):
+#     print(f"{word} is a palindrome.")
+# else:
+#     print(f"{word} is not a palindrome.")
