d051c86ce583380321f72c654283259df19f1c77
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-20 15:58:37 +0530

    remove commented-out factorial method and its input prompt

--- tasks/factorial.py
+++ tasks/factorial.py
@@ -1,18 +1,5 @@
 ##_____Write a function to print a factorial numbers for a given input range (2 methods )
 #Method_1
-# def factorial(n):
-#     i = n - 1
-#     org = n
-#     while i > 0:
-#         n = n * i
-#         i -=  1
-#     print(f"{n} is a factorial of {org}")
-    
-
-# n = int(input("Enter a number: "))
-# factorial(n)
-
-# #Method_2
 def fact(n):
     org = n
     i = n - 1
