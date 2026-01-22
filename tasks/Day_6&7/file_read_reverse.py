cfe6cba59ddeb2051ff89487f3043a9494ed4a61
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-22 15:29:23 +0530

    created file handling in reverse.

---
+++ tasks/Day_6&7/file_read_reverse.py
@@ -0,0 +1,8 @@
+"""
+File read operations (Read one file - print the file content in reverse order) (2 methods)
+
+(in file)
+Input:- Hi file handling in reverse
+
+Output: iH elif gnildnah ni esrever
+"""
