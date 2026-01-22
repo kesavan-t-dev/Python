f2125f6e84deb340858cab2027f93fbf86c9c4a9
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-22 20:44:40 +0530

    added method_1 and method_2

---
+++ tasks/Day_6&7/perfect_square.py
@@ -0,0 +1,42 @@
+"""
+Given a positive integer num, write a function that returns True if num is a perfect square else False. (2 methods)
+"""
+
+#Method_1
+# def perfect_square(num):
+
+#     if num < 1:
+#         return False
+
+#     left, right = 1, num
+#     while left <= right:
+#         mid = (left + right) // 2
+#         square = mid * mid
+
+#         if square == num:
+#             return True
+#         elif square < num:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+
+#     return False
+
+# n = int(input("Enter a number:"))
+# print(perfect_square(n))
+
+#Method_2
+def perfect_square(num):
+
+    if num < 1:
+        return False
+
+    odd = 1
+    while num > 0:
+        num -= odd
+        odd += 2
+
+    return num == 0
+
+n = int(input("Enter a number:"))
+print(perfect_square(n))
