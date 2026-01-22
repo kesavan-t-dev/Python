f1bb6baff52b9db39d3031212eed00dff20011f2
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-22 15:03:27 +0530

    added method_1

--- tasks/Day_4&5/roman_to_numbers.py
+++ tasks/Day_4&5/roman_to_numbers.py
@@ -10,14 +10,21 @@ def romanToInt(s):
     roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
     i = 0
     num = 0
-    while i < len(s):
-        if i+1<len(s) and s[i:i+2] in roman:
+    n = len(s)
+    print(s,"S ")
+    print(n,"N LEN OF S")
+    while i < n:
+        if i+1< n and s[i:i+2] in roman:
+            print(s[i:i+2])
             num+=roman[s[i:i+2]]
+            print(num,"IF NUM VALUE")
             i+=2
+            print(i)
         else:
-        #print(i)
             num+=roman[s[i]]
+            print(num,"ELSE NUM VALUE")
         i+=1
+    print(num)
     return num
 
 s = input("Enter a Roman Letter(XIV):")
