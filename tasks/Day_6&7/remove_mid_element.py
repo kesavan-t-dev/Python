16d37002cd1b7ac0511ee3e25e70a84df570af98
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-22 18:55:13 +0530

    added Method_1 for remove mid using shift next value for the mid index

--- tasks/Day_6&7/remove_mid_element.py
+++ tasks/Day_6&7/remove_mid_element.py
@@ -1,5 +1,31 @@
 """
-Write a program about remove the mid of element in given array. (2 methods)
+Write a program about remove the mid of element in given array. (2 methods) 
 
 """
+#Method_1 index based shift next value to the mid index 
+def remove_middle_shift(arr):
+    n = len(arr)
+    if n == 0:
+        return arr
+
+    mid_index = n // 2  
+    new_arr = [0] * (n - 1)  
+
+    j = 0
+    for i in range(n):
+        if i != mid_index:
+            new_arr[j] = arr[i]
+            j += 1
+    return new_arr
+
+
+
+a = []
+b = int(input("Enter a range: "))
+for i in range(b):
+    val = input(f"Enter value: ")
+    a.append(val)
+
+result = remove_middle_shift(a)
+print(result)
 
