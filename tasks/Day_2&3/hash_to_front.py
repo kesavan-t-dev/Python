4b78ef68a1f37faba744835770fb13911c2f0407
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-23 10:29:08 +0530

    removed method_1 and added new method

--- tasks/Day_2&3/hash_to_front.py
+++ tasks/Day_2&3/hash_to_front.py
@@ -6,76 +6,50 @@ Input: Move#Hash#to#Front
 Output: ###MoveHashtoFront
 """
 
-#Method_1 using bubble sort little bit slow based on the input it takes more time TC - O(N^2)&SC - O(N) 
-# class BubbleSort:
-#     def move_hashes_to_front(self, s):
-#         chars = list(s)
-#         n = len(chars)
+#____________________________________________________________________________________________
+#Method_1 using insertion sort
+# def move_hash(s):
+#     arr = list(s)  
+#     n = len(arr)
+#     print(n)
+#     for i in range(n):
+#         j = i
+#         while j > 0 and arr[j] == '#':
+#             a = arr[j], arr[j - 1] = arr[j - 1], arr[j]
+#             # print(a)
+#             j -= 1
+#         # print(i, j, arr)
+#     return ''.join(arr)  
 
-#         for i in range(n):
-#             # print(i) 
-#             for j in range(n - 1, i, -1):
-#                 # print(j)
-#                 if chars[j] == '#' and chars[j - 1] != '#':
-#                     a = chars[j], chars[j - 1] = chars[j - 1], chars[j]
-#                     # print(chars[j],chars[j - 1])
-#                     # print(a)
-#             # print(i,j,chars)                
-#         return "".join(chars)
+# text = "Move#Hash#to#Front"
+# # text = input("Enter a string/Number with hashstags:")
 
-
-# sorter = BubbleSort()
-
-# input_str = "Move#Hash#to#Front"
-# # input_str = input("Enter a string/Number with hashstags:")
-# print("\nBefore : ",input_str)
-# result = sorter.move_hashes_to_front(input_str)
-# print("After : ",result)
+# print("Before:", text)
+# print("After: ", move_hash(text))
 
 #____________________________________________________________________________________________
-# #Method_2 using insertion sort
-def move_hash(s):
-    arr = list(s)  
-    n = len(arr)
-    print(n)
-    for i in range(n):
-        j = i
-        while j > 0 and arr[j] == '#':
-            a = arr[j], arr[j - 1] = arr[j - 1], arr[j]
-            # print(a)
-            j -= 1
-        # print(i, j, arr)
-    return ''.join(arr)  
-
-text = "M##M"
-# text = input("Enter a string/Number with hashstags:")
-
-print("Before:", text)
-print("After: ", move_hash(text))
-
-#____________________________________________________________________________________________
-# #Method_3 using new list to store and append that string
+# #Method_2 using new list to store and append that string
 # def move_hash_append(s):
-#     result = []
+#     result = ""
 #     hash_count = 0
     
 #     for ch in s:
 #         if ch == '#':
 #             hash_count += 1
     
-#     for _ in range(hash_count):
-#         result.append('#')
+    
+#     result = '#' *hash_count
     
 #     for ch in s:
 #         if ch != '#':
-#             result.append(ch)
-    
-#     return "".join(result)
+#             result += ch
+#     # print(result)
+#     return result
 
 # print("\nBefore: Move#Hash#to#Front ", "\nafter:",move_hash_append("Move#Hash#to#Front"))
 
 #____________________________________________________________________________________________
-# #Method_4 using slicing from left to right store them 
+# #Method_3 using slicing from left to right store them 
 # def move_hash_reverse_fill(s):
 #     n = len(s)
 #     result = [""] * n 
@@ -95,3 +69,25 @@ print("After: ", move_hash(text))
 #     return "".join(hash_part + non_hash_part)
 
 # print("\nBefore: Move#Hash#to#Front ", "\nafter:",move_hash_reverse_fill("Move#Hash#to#Front"))
+
+#________________________________________________________________________________________________________
+#Method_4
+def move_hash_reverse(s):
+    n = len(s)
+    result = [""] * n
+    left = 0           
+    right = left + s.count('#')  
+
+    for ch in s:
+        if ch == '#':
+            result[left] = '#'
+            left += 1
+        else:
+            result[right] = ch
+            right += 1
+
+    return "".join(result)
+
+
+print("\nBefore: Move#Hash#to#Front",
+      "\nAfter :", move_hash_reverse("Move#Hash#to#Front"))
