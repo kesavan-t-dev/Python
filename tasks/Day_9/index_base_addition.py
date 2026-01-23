d0f6d290a6c040db6f9d72e1a163710284f07ec6
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-23 13:16:06 +0530

    method _1 and 2 added

---
+++ tasks/Day_9/index_base_addition.py
@@ -0,0 +1,41 @@
+"""
+Input: [1234, 567, 89, 10]
+output: [10, 18, 17, 1] (2 methods) 
+"""
+
+#Method_1
+# def sum_of_digits_while(num):
+#     num = abs(num)
+#     total = 0
+#     while num > 0:
+#         digit = num % 10  
+#         total += digit
+#         num //= 10        
+#     return total
+
+
+# arr = [1234, 567, 89, 10]
+# result = []
+# print("Input",arr)
+# for n in arr:
+#     result.append(sum_of_digits_while(n))
+
+# print("Output:", result)
+
+
+# Method 2:
+def sum_digits_loop(numbers):
+    def sum_digits(n):
+        n = abs(n)  
+        total = 0
+        while n > 0:
+            total += n % 10
+            n //= 10
+        return total
+    return [sum_digits(num) for num in numbers]
+
+
+nums = [1234, 567, 89, 10]
+print("Input:",nums)
+print("Output :", sum_digits_loop(nums))
+
