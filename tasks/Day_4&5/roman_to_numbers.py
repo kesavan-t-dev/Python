66f33feb6747bfed706065ccdb0ffaccf00cc4c6
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-23 16:30:03 +0530

    give some space and change snake cases

--- tasks/Day_4&5/roman_to_numbers.py
+++ tasks/Day_4&5/roman_to_numbers.py
@@ -4,7 +4,7 @@ Write a function that function usage is convert roman_charaters to integer (1 me
 
 #Method_1
 try:
-    def romanToInt(s):
+    def roman_to_int(s):
         roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
         i = 0
         num = 0
@@ -14,18 +14,18 @@ try:
         while i < n:
             if i+1< n and s[i:i+2] in roman:
                 # print(s[i:i+2])
-                num+=roman[s[i:i+2]]
+                num += roman[s[i:i+2]]
                 # print(num,"IF NUM VALUE")
                 i+=2
                 # print(i)
             else:
-                num+=roman[s[i]]
+                num += roman[s[i]]
                 # print(num,"ELSE NUM VALUE")
-                i+=1
+                i += 1
         # print(num)
         return num
 
     s = input("Enter a Roman Letter(XIV):")
-    print(romanToInt(s))
+    print(roman_to_int(s))
 except KeyError as e:
     print("give Roman values - (I V X L C D M) with Correct Combination",e)
