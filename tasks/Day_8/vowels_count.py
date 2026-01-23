ff1b01d6c83770e618de07deaaa1948248e40854
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-23 12:12:03 +0530

    method_2 added

--- tasks/Day_8/vowels_count.py
+++ tasks/Day_8/vowels_count.py
@@ -3,19 +3,57 @@ Write a code to get the count of vowels in given string (2 methods) no inbuilt f
 """
 
 #Method_1
-def count_vowels_for_loop(text):
-    vowels = "aeiouAEIOU"  
+# def count_vowels(text):
+#     vowels = "aeiouAEIOU"  
+#     count = 0
+#     for ch in text:
+#         # try:
+#         #     int(ch)
+#         #     print(f"Number in string: {ch}")
+#         #     continue  
+#         # except ValueError:
+#             if ch in vowels:
+#                 count += 1
+#     return count
+    
+
+# user_input = input("Enter a string: ")
+# print("Vowel count:", count_vowels(user_input))
+
+#Method_2 using case
+def count_vowels(text):
     count = 0
     for ch in text:
         # try:
         #     int(ch)
-        #     print(f"Number in string: {ch}")
+        #     print(f"Number found in string: {ch}")
         #     continue  
         # except ValueError:
-            if ch in vowels:
-                count += 1
+            match ch:
+                case 'a':
+                    count += 1
+                case 'A':
+                    count += 1
+                case 'e':
+                    count += 1
+                case 'E':
+                    count += 1
+                case 'i':
+                    count += 1
+                case 'I':
+                    count += 1
+                case 'o':
+                    count += 1
+                case 'O':
+                    count += 1
+                case 'u':
+                    count += 1
+                case 'U':
+                    count += 1
+                case _:
+                    pass
     return count
-    
+
 
 user_input = input("Enter a string: ")
-print("Vowel count:", count_vowels_for_loop(user_input))
+print("Vowel count:", count_vowels(user_input))
