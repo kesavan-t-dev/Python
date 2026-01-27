c6a267322f5cf63660277afee6b69fc13114e3e3
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-27 13:29:07 +0530

    change the logic for method_1 simple but not efficient

--- tasks/Day_4&5/consecutive_letter_numbers.py
+++ tasks/Day_4&5/consecutive_letter_numbers.py
@@ -8,54 +8,44 @@ Output: a2b4e4f2g3
 """
 
 #Method_1 count and reset count 
-# def compress_string(s):
-#     if not s:
-#         return ""
-
-#     chars, counts = [], []
-#     count = 1
-
-#     for i in range(1, len(s)):
-#         if s[i] == s[i - 1]:
-#             count += 1
-#         else:
-#             if s[i - 1] in chars:
-#                 counts[chars.index(s[i - 1])] += count
-#             else:
-#                 chars.append(s[i - 1])
-#                 counts.append(count)
-#             count = 1
-
-#     if s[-1] in chars:
-#         counts[chars.index(s[-1])] += count
-#     else:
-#         chars.append(s[-1])
-#         counts.append(count)
-
-#     return "".join(chars[i] + str(counts[i]) for i in range(len(chars)))
-
-# input = input("Enter:")
-# print(compress_string(input)) #output : a3b2c1132231
-
-
-#Method_2 
 def compress_string(s):
-    if not s: return ""
-    chars, counts = [], []
-    n = len(chars)
-    for ch in s:
-        idx = -1
-        for i in range(n):
-            if chars[i] == ch: idx = i; break
-        if idx != -1:
-            counts[idx] += 1
-        else:
-            chars.append(ch)
-            counts.append(1)
-    return "".join(chars[i] + str(counts[i]) for i in range(n))
+    if not s:
+        return ""
 
-n = input("enter a word:")
-print(compress_string(n)) #output : a3b2c1132231
+    result = ""
+    for ch in s:
+        if ch not in result:
+            count = 0
+            for c in s:
+                if c == ch:
+                    count += 1
+            result += ch + str(count)
+
+    return result
+
+input = input("Enter:")
+print(compress_string(input)) #output : a3b2c1132231
+
+
+# #Method_2 
+# def compress_string(s):
+#     chars, counts = [], []
+#     n = len(chars)
+#     for ch in s:
+#         idx = -1
+#         for i in range(n):
+#             if chars[i] == ch: 
+#                 idx = i; 
+#                 break
+#         if idx != -1:
+#             counts[idx] += 1
+#         else:
+#             chars.append(ch)
+#             counts.append(1)
+#     return "".join(chars[i] + str(counts[i]) for i in range(n))
+
+# n = input("enter a word:")
+# print(compress_string(n)) #output : a3b2c1132231
 
 #Method_3
 # def compress_stack(s):
@@ -72,5 +62,6 @@ print(compress_string(n)) #output : a3b2c1132231
 
 #     result = "".join(ch + str(cnt) for ch, cnt in stack)
 #     return result
+
 # n = input("enter a word:")
 # print(compress_stack(n))  
