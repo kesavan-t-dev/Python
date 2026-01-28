0902512284cafa7952c1b00a62667ad072268f2c
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-28 19:01:38 +0530

    change new methods with optimized

--- tasks/Day_9/index_base_addition.py
+++ tasks/Day_9/index_base_addition.py
@@ -4,38 +4,35 @@ output: [10, 18, 17, 1] (2 methods)
 """
 
 #Method_1
-# def sum_of_digits_while(num):
-#     num = abs(num)
-#     total = 0
-#     while num > 0:
-#         digit = num % 10  
-#         total += digit
-#         num //= 10        
-#     return total
+def sum_of_digits_while(num):
+    num = abs(num)
+    total = 0
+    while num > 0:
+        digit = num % 10  
+        total += digit
+        num //= 10        
+    return total
 
 
-# arr = [1234, 567, 89, 10]
-# result = []
-# print("Input",arr)
-# for n in arr:
-#     result.append(sum_of_digits_while(n))
+arr = [1234, 567, 89, 10]
+result = []
+print("Input",arr)
+for n in arr:
+    result.append(sum_of_digits_while(n))
 
-# print("Output:", result)
+print("Output:", result)
 
 
 # Method 2:
-def sum_digits_loop(numbers):
-    def sum_digits(n):
-        n = abs(n)  
-        total = 0
-        while n > 0:
-            total += n % 10
+def sum_digits_prealloc(numbers):
+    results = [0] * len(numbers)
+    for i in range(len(numbers)):
+        n = abs(numbers[i])
+        while n:
+            results[i] += n % 10
             n //= 10
-        return total
-    return [sum_digits(num) for num in numbers]
-
+    return results
 
 nums = [1234, 567, 89, 10]
-print("Input:",nums)
-print("Output :", sum_digits_loop(nums))
-
+print("Input:", nums)
+print("Output:", sum_digits_prealloc(nums)) 
