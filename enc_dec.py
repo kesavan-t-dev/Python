a5b68d3aa85d3c2aee97763ae31cff8c0516445f
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-01-31 12:12:07 +0530

    testing file creation with encrypt and decryption using secure key store in seperate file

--- enc_dec.py
+++ enc_dec.py
@@ -1,11 +1,55 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad, unpad
+"""
+encryption and decryption using three libraries
+"""
 
-key = get_random_bytes(16)
-cipher = AES.new(key, AES.MODE_CBC)
-data = b"Secret Message"
-ciphertext = cipher.encrypt(pad(data, AES.block_size))
-cipher_dec = AES.new(key, AES.MODE_CBC, cipher.iv)
-plaintext = unpad(cipher_dec.decrypt(ciphertext), AES.block_size)
-print("Decrypted:", plaintext.decode())
+#-----------------------#
+#  fernet cryptography  #
+#-----------------------#
+# Fernet module 
+from cryptography.fernet import Fernet
+
+# key is generated
+key = Fernet.generate_key()
+print("key value:",key)
+# value of key is assigned to a variable
+f = Fernet(key)
+print("f value:",f)
+# the plaintext is converted to ciphertext
+token = f.encrypt(b"welcome to geeksforgeeks")
+
+# display the ciphertext
+print("token value after encrypt:",token)
+
+# decrypting the ciphertext
+d = f.decrypt(token)
+
+# display the plaintext
+print(d.decode())
+
+
+"""
+
+#-----------------------#
+#  AES cryptography     #
+#-----------------------#
+
+from cryptography.fernet import Fernet
+
+# Generate a key
+key = Fernet.generate_key()
+cipher = Fernet(key)
+
+# Encrypt
+text = b"abcde"
+print("byte code values are",list(text))
+text = "Encryption"
+encrypted = cipher.encrypt(text.encode())
+
+
+# Decrypt
+decrypted = cipher.decrypt(encrypted).decode()
+
+print("Key:", key)
+print("Encrypted:", encrypted)
+print("Decrypted:", decrypted)"""
+
